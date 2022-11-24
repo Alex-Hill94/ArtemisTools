710af33782e83e74c18f0e18cc22772aacaf7ea2
Alexander Hill <32838443+Alex-Hill94@users.noreply.github.com>
2022-11-24 15:58:15 +0000

    Update LoadData.py

--- LoadData.py
+++ LoadData.py
@@ -3,7 +3,7 @@ import numpy as np
 import eagle_IO.eagle_IO as E
 import scipy as sp
 import h5py as h5
-import illustris_python as il
+#import illustris_python as il
 
 class LoadData_EAGLE():
     
@@ -11,8 +11,6 @@ class LoadData_EAGLE():
         self.box = box
         self.tag = tag
         self.simulation = "/beegfs2/hpcdata0_backup/simulations/EAGLE/%s/data" % box
-        self.alexs_simulation_tensor_path = '/hpcdata7/ariahill/data/%s/%s/tensors5.hdf5' % (box, tag)
-        self.alexs_simulation_density_path = '/hpcdata7/ariahill/data/%s/%s/rho_measures_temp.hdf5' % (box, tag)
         self.dmo = dmo
 
     def query_eagle_table_array(self, qois, noH = True, physicalUnits = True, verbose = True):
@@ -41,91 +39,6 @@ class LoadData_EAGLE():
             data.append(data_temp)
         self.data_attribute = data
 
-    def query_alexs_tensor_data(self, qois, matter, scale, aperture):
-        data = []
-        u = h5.File(self.alexs_simulation_tensor_path, 'r')
-        for i in range(0, len(qois)):
-            qoi = qois[i]            
-            data_temp = u['%s/%s/%s/%s' % (matter, scale, aperture, qoi)][:].astype('float64')
-            data.append(data_temp)
-        u.close()
-        self.data_alex_tensor = data
-
-    def query_alexs_density_data(self, qois, sampling, value):
-        data = []
-        u = h5.File(self.alexs_simulation_density_path, 'r')
-        for i in range(0, len(qois)):
-            qoi = qois[i]            
-            data_temp = u['%s/cut_%s/%s' % (sampling, value, qoi)][:].astype('float64')
-            data.append(data_temp)
-        u.close()
-        self.data_alex_density = data
-
-    def query_rho_data_density_sample(self,qois, aperture_30kpc = True, focus_llim = 8.5, ref_llim = 9.5, centrals_only = False):
-        ''' 
-        d_3_to_6                 Dataset {4, 22722}
-        m_star_focus             Dataset {22722}
-        m_star_reference         Dataset {7314}
-        n_halo_3_to_6            Dataset {4, 22722}
-        read_me                  Dataset {SCALAR}
-        rho_3_to_6               Dataset {4, 22722}
-        subhalo_IDs_focus        Dataset {22722}
-        subhalo_IDs_reference    Dataset {7314}
-        '''
-        self.centrals_only = centrals_only
-
-        if self.centrals_only:
-            cent_path  = '/centrals_only'
-            print('Centrals Only')
-        elif self.centrals_only == False:
-            cent_path = '/all_objects'
-            print('All Objects')
-
-        u = h5.File(self.alexs_simulation_density_path, 'r')
-        if self.dmo == False:
-            print('Loading rho_N data for density sample, selected above %s for the focus sample and above %s for the reference sample. Aperture_30kpc = %s' % (focus_llim, ref_llim, aperture_30kpc))
-            data = []
-            if aperture_30kpc:
-                mass_arg = 'm_star_within_30kpc'
-            else:
-                mass_arg = 'm_star_within_30kpc'            
-            for i in range(0, len(qois)):
-                qoi = qois[i]            
-                data_temp = u['galaxies/density_selected%s/%s/masses_from_%s_%s/%s' % (cent_path, mass_arg, focus_llim, ref_llim, qoi)][:].astype('float64') 
-                data.append(data_temp)
-        elif self.dmo == True:
-            print('Loading rho_N data for density sample (DMO), selected above %s for the focus sample and above %s for the reference sample.' % (focus_llim, ref_llim))
-            data = []
-            mass_arg = 'm_subhalo'
-            for i in range(0, len(qois)):
-                qoi = qois[i]            
-                data_temp = u['galaxies/density_selected%s/%s/masses_from_%s_%s/%s' % (cent_path, mass_arg, focus_llim, ref_llim, qoi)][:].astype('float64') 
-                data.append(data_temp)
-        u.close()
-        self.data_alex_density_samp = data
-
-    def query_rho_data_mass_sample(self,qois, aperture_30kpc = True, focus_llim = 8.5):
-        '''
-        d_3_to_6                 Dataset {4, 22722}
-        m_star                   Dataset {22722}
-        n_halo_3_to_6            Dataset {4, 22722}
-        read_me                  Dataset {SCALAR}
-        rho_3_to_6               Dataset {4, 22722}
-        subhalo_IDs              Dataset {22722}
-        '''
-        print('Loading rho_N data for mass sample, selected above %s. Aperture_30kpc = %s' % (focus_llim, aperture_30kpc))
-        data = []
-        if aperture_30kpc:
-            mass_arg = 'm_star_within_30kpc'
-        else:
-            mass_arg = 'm_star_within_30kpc'
-        u = h5.File(self.alexs_simulation_density_path, 'r')
-        for i in range(0, len(qois)):
-            qoi = qois[i]            
-            data_temp = u['galaxies/mass_selected/%s/%s/%s' % (mass_arg, focus_llim, qoi)][:].astype('float64') 
-            data.append(data_temp)
-        u.close()
-        self.data_alex_mass_samp = data
 
     def query_partdata(self, qois, noH = True, physicalUnits = True, verbose = True):
         data = []
@@ -258,16 +171,6 @@ class LoadData_TNG():
         fields_sub = qois
         haloes = il.groupcat.loadHalos(self.simulation,self.tag,fields=fields_sub)
         self.data_grp = haloes
-
-    def query_jon_data(self, qois):
-        path = "/beegfs2/hpcdata0_backup/arijdav/TNG_catalogues/%s/snap_0%s/catalogue.hdf5" % (self.box, self.tag)
-        u = h5.File(path, 'r')
-        data = []
-        for i in range(0, len(qois)):
-            qoi = qois[i]            
-            data_temp = u['%s' % qoi][:].astype('float64')
-            data.append(data_temp)
-        self.jon_data = data
 
 class LoadDataBAHAMAS_Special():
     # What is this for?
